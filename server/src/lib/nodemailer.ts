34d01723dcd12eecff7d85e8ab5a2460594b58d3
leonardo.zoot@gmail.com <leonardo.zoot@gmail.com>
2026-01-29 07:57:27 -0300

    nodemailer transporter configured

--- server/src/lib/nodemailer.ts
+++ server/src/lib/nodemailer.ts
@@ -1,25 +1,21 @@
 import nodemailer from "nodemailer";
 import type SMTPTransport from "nodemailer/lib/smtp-transport";
 
-const transporterOptions: SMTPTransport.Options =
-  process.env.NODE_ENV === "test"
-    ? {
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT, 10) : 587,
-        secure: false,
-        auth: {
-          user: process.env.ETHEREAL_USER,
-          pass: process.env.ETHEREAL_PASS,
-        },
-      }
-    : {
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT, 10) : 587,
-        secure: process.env.SMTP_SECURE === "true",
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS,
-        },
-      };
+export const transporterOptions: SMTPTransport.Options = {
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT, 10) : 587,
+  secure: process.env.SMTP_SECURE === "true",
+  auth: {
+    user:
+      process.env.NODE_ENV === "test"
+        ? process.env.ETHEREAL_USER
+        : process.env.SMTP_USER,
+    pass:
+      process.env.NODE_ENV === "test"
+        ? process.env.ETHEREAL_PASS
+        : process.env.SMTP_PASS,
+  },
+};
 
-export const transporter = nodemailer.createTransport(transporterOptions);
+export const createTransporter = () =>
+  nodemailer.createTransport(transporterOptions);
